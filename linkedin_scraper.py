9a413e5abd7bfec0d141f52b4691855a4524783f
JedSawicki <95547700+JedSawicki@users.noreply.github.com>
2022-03-15 20:52:35 +0100

    testing scraping

--- linkedin_scraper.py
+++ linkedin_scraper.py
@@ -2,13 +2,39 @@ from requests_html import HTMLSession
 
 
 class Scraper:
-    def __init__(self, tags):
-        self.tag = tags
-        self.links = f'https://pl.linkedin.com/jobs/{self.tag}'
+    def __init__(self):
+        self.domain = f'https://pl.linkedin.com/jobs/'
 
-    def linkedin_worker(self):
+
+    def linkedin_worker(self, tag: str):
         s = HTMLSession()
-        r = s.get(self.links)
+        r = s.get(str(self.domain + tag))
+        # r = s.get(str(self.experimental_domain))
+        print(r.status_code)
+        urllist = []
+
+        jobs = r.html.find('section.two-pane-serp-page__results-list')
+
+        for j in jobs:
+            names = j.find('span.screen-reader-text')
+            for name in names:
+                # dictionary method
+                index = names.index(name)
+                item = {
+                    'name': j.find('span.screen-reader-text')[index].text.strip(),
+                    'href': j.find('a.base-card__full-link')[index].absolute_links,
+                    'company_name': j.find('h4.base-search-card__subtitle')[index].text.strip(),
+                    'location': j.find('span.job-search-card__location')[index].text.strip()
+                }
+                urllist.append(item)
+
+        print(urllist)
+        return urllist
+
+    def custom_linkedin_worker(self, key1: str, key2: str, key3: str):
+        s = HTMLSession()
+        experimental_domain = f'https://pl.linkedin.com/jobs/search?keywords={key1}%20{key2}%20{key3}'
+        r = s.get(str(experimental_domain))
         print(r.status_code)
         urllist = []
 
@@ -31,6 +57,6 @@ class Scraper:
         return urllist
 
 
-# jobs = Scraper('junior-python-jobs')
-#
-# jobs.linkedin_worker()
+jobs = Scraper()
+
+jobs.linkedin_worker('junior-python-jobs')
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+from enum import Enum
+
+import uvicorn
 from fastapi import FastAPI, HTTPException
 
 from linkedin_scraper import Scraper
 
 app = FastAPI()
+scrapy = Scraper()
+
+
+class ModelName(str, Enum):
+    first = "junior"
+    resnet = "resnet"
+    lenet = "lenet"
 
 
 @app.get("/")
@@ -10,7 +20,29 @@ async def root():
     return {"message": "Hello World"}
 
 
-@app.get("/{job}")
-async def read_item(job):
-    jobs = Scraper('junior-python-jobs')
-    return jobs.linkedin_worker()
+@app.get("/dbjobs/{model_name}")
+async def read_item(model_name: ModelName):
+    # junior-python-jobs
+    if model_name == ModelName.first:
+        return scrapy.custom_linkedin_worker(model_name.value, '', '')
+
+
+@app.get("/{jobs}")
+async def read_item(jobs):
+    return scrapy.linkedin_worker('junior-python-jobs')
+
+
+@app.get("/linkedin/{job}")
+async def read_item(job: str):
+    # junior-python-jobs
+    return scrapy.linkedin_worker(job)
+
+
+@app.get("/customjobs/{test1}")
+async def read_item():
+    # junior-python-jobs
+    return scrapy.custom_linkedin_worker('senior', 'tester', '')
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
