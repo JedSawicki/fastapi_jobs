ef7234c0b4aae88047f583600d77ada34ac1a4b1
JedSawicki <jedsawicki@gmail.com>
2022-03-15 23:03:04 +0100

    working multi-parameter linkedin scraper api

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from enum import Enum
 
 import uvicorn
@@ -9,22 +11,9 @@ app = FastAPI()
 scrapy = Scraper()
 
 
-class ModelName(str, Enum):
-    first = "junior"
-    resnet = "resnet"
-    lenet = "lenet"
-
-
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/dbjobs/{model_name}")
-async def read_item(model_name: ModelName):
-    # junior-python-jobs
-    if model_name == ModelName.first:
-        return scrapy.custom_linkedin_worker(model_name.value, '', '')
 
 
 @app.get("/{jobs}")
@@ -32,16 +21,10 @@ async def read_item(jobs):
     return scrapy.linkedin_worker('junior-python-jobs')
 
 
-@app.get("/linkedin/{job}")
-async def read_item(job: str):
+@app.get("/linkedin/{item_name}")
+async def read_item(item_name: str, item_name2: Optional[str] = None, item_name3: Optional[str] = None):
     # junior-python-jobs
-    return scrapy.linkedin_worker(job)
-
-
-@app.get("/customjobs/{test1}")
-async def read_item():
-    # junior-python-jobs
-    return scrapy.custom_linkedin_worker('senior', 'tester', '')
+    return scrapy.custom_linkedin_worker(item_name, item_name2, item_name3)
 
 
 if __name__ == "__main__":
